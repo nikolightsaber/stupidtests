bcb8b8a408d283dedfb97d964b43f03e9de36222
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2023-02-15 16:32:18 +0100

    fail

---
+++ cs_comp_tuple/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace cs_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int, int) a = (1 ,1);
+            (int, int) b = (1 ,2);
+            Console.WriteLine("{0}", a < b);
+        }
+    }
+}
