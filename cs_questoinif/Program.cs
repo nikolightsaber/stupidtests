3fdfe4a399e4487b6c663bc42e3d16f32a758528
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2023-03-07 10:47:09 +0100

    question

---
+++ cs_questoinif/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        class Test
+        {
+            public bool Enabled = false;
+        }
+        static void Main(string[] args)
+        {
+            Test toz = null;
+            Test toz2 = new Test();
+
+            if (!toz?.Enabled ?? false)
+                Console.WriteLine("works1");
+
+            if (!toz2?.Enabled ?? false)
+                Console.WriteLine("2works1");
+
+            if (!(toz?.Enabled ?? true))
+                Console.WriteLine("works2");
+
+            if (!(toz2?.Enabled ?? true))
+                Console.WriteLine("2works2");
+
+        }
+    }
+}
